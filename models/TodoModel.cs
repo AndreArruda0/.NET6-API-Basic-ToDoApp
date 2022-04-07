0e8e1f734ce5a1f0129b7eb6e5ff3c06008c5ec3
FIgit config --global user.email MY_NAME@example.com <MY_NAME@example.com>
2022-04-06 23:46:33 -0300

    Initial commit

---
+++ models/TodoModel.cs
@@ -0,0 +1,15 @@
+namespace Todo.Models
+{
+
+    public class TodoModel
+    {
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Done { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
